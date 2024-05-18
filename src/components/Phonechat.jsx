0d550da63f9f1128b37a6e3d45fe10f7edc7ee31
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-05-18 16:29:24 +0530

    Update Phonechat.jsx

--- src/components/Phonechat.jsx
+++ src/components/Phonechat.jsx
@@ -79,7 +79,7 @@ const Phonechat = () => {
   }
 
   return (
-    <div className='relative sm:hidden w-[95%]  bg-white pb-[5%] sm:w-[30%] h-screen  p-[0.25%] flex flex-col items-center '>
+    <div className='relative sm:hidden w-[95%]  bg-white pb-[5%] sm:w-[30%] h-full  p-[0.25%] flex flex-col items-center '>
       <div className='mb-[4%] absolute top-0 w-[95%] bg-white border-b-2 flex justify-center items-center p-[1%]'>
           <div className='w-[20%]'>
           <img
