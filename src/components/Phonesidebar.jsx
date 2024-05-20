9e401395f848257976214e65b6f445967fa796fc
Naman8042 <131790382+Naman8042@users.noreply.github.com>
2024-05-20 18:31:53 +0530

    Update Phonesidebar.jsx

--- src/components/Phonesidebar.jsx
+++ src/components/Phonesidebar.jsx
@@ -23,7 +23,7 @@ function Sidebar() {
     get()
     },[data])
   return (
-    <div className='h-screen overflow-x-hidden overflow-y-hidden'>
+    <div className='h-screen overflow-x-hidden overflow-y-hidden py-[2%]'>
       <div className='h-[15%] overflow-x-hidden'>
       <Phonenavbar/> 
       </div>
@@ -35,7 +35,7 @@ function Sidebar() {
        className='b-none text-bg ml-[1%] p-[1%]'
        />
       </div>
-      <div className=' bg-white flex h-[75%] overflow-y-auto overflow-x-hidden flex-col p-[1%]  m-[3%] rounded-3xl'>
+      <div className=' bg-white flex h-[72%] overflow-y-auto overflow-x-hidden flex-col p-[1%]  m-[3%] rounded-3xl'>
         {
             data.map((conversation)=>{
               if(conversation.isGroupChat===false){
